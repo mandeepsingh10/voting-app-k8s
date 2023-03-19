3c8f224059d201ad589508d3302f66e113bc2905
mandeepsingh10 <mandeepsingh1018@gmail.com>
2023-03-19 19:25:16 +0530

    Added setup.sh to setup pods and services for kodekloud lab on k8s- example voting app

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+apt install git -y
+git clone https://github.com/mandeepsingh10/voting-app-k8s.git
+
+cd voting-app-k8s
+kubectl create -f pods/voting-app.yaml
+kubectl create -f services/voting-app-service.yaml
+
+kubectl create -f pods/redis-pod.yaml
+kubectl create -f services/redis-service.yaml
+
+kubectl create -f pods/postgres-pod.yaml
+kubectl create -f services/postgres-service.yaml
+
+kubectl create -f pods/worker-pod.yaml
+
+kubectl create -f pods/result-app-pod.yaml
+kubectl create -f services/result-app-service.yaml
+
+kubectl get pods,svc
+
